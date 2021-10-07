6ae277cd84efab0a3058d3510ce1e0725b5776c7
carinasylvester <carina.sylvester@outlook.com>
2021-10-07 12:27:04 -0400

    Installed dependencies

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Component that renders application
+ReactDOM.render(<App />, document.getElementById('root'));
+
+import App from './App';
+
