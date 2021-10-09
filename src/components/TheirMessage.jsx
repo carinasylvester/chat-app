866e55e9a04c46deea9830d50d925b56d19fc34e
carinasylvester <carina.sylvester@outlook.com>
2021-10-09 18:34:17 -0400

    Modified other users' messages

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,11 +1,33 @@
 // Messages sent by others
 
 const TheirMessage = () => {
+    // determine whether this is the first message by a user
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
     return (
-        <div>
-            TheirMessage
+        <div className="message-row">
+            {isFirstMessageByUser && (
+                <div
+                    className="message-avatar"
+                    style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
+                />
+            )}
+            {/* if message is an image... */}
+            {message.attachments && message.attachments.length > 0
+                ? (
+                    <img
+                        src={message.attachments[0].file}
+                        alt="message-attachment"
+                        className="message-image"
+                        style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                    />
+                )
+                : ( // if message is text... 
+                    <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+                        {message.text}
+                    </div>
+                )}
         </div>
-    )
-}
+    );
+};
 
-export default TheirMessage
+export default TheirMessage;
